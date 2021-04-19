8b5f8176aa44d0da7b1d55adda86379757f61d4e
jlazar17 <jeff.p.lazar@gmail.com>
2021-04-19 11:09:57 -0500

    change to include env var

--- event_selection/l3_b/process-l3_b.sh
+++ event_selection/l3_b/process-l3_b.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 export PYTHONPATH=$PYTHONPATH:/data/user/jlazar/.pylib
-source /home/jlazar/golemsolarwimp.sh
+BASEDIR=$SOLAR_BASE_DIR
+source $BASEDIR/env_nocombo.sh
 
 for INPUTFILE in "$@";
 do
     for INFILE in `cat $INPUTFILE`
     do
-        /data/user/jlazar/combo37/build/env-shell.sh python $SOLAR_INSTALL_DIR/solar_atmospherics/event_selection/l3_b/process-l3_b.py -i ${INFILE};
+        /data/user/jlazar/combo37/build/env-shell.sh python $BASEDIR/event_selection/l3_b/process-l3_b.py -i ${INFILE};
     done
 done
