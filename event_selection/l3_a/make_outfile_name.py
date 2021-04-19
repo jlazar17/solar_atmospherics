f7681479805ed1c05fac54b85631406431aad769
jlazar17 <jeff.p.lazar@gmail.com>
2021-04-19 13:36:54 -0500

    Make outfile from env var

--- event_selection/l3_a/make_outfile_name.py
+++ event_selection/l3_a/make_outfile_name.py
@@ -1,37 +1,38 @@
 import os
+
+savedir = os.environ.get('L3_A_SAVEDIR')
+
 def make_outfile_name(infile):
     infile  = infile.replace('=', '').replace('\\', '')
-    print(infile)
     if 'nancy001' in infile:
-        outdir    = '/data/user/jvillarreal/solar_atmospherics/event_selection/l3_a/data/nancy/i3/'
+        outdir    = '%s/nancy/i3/' % savedir
         nutype    = infile.split('/')[9]
         eregime   = infile.split('/')[10].replace('_', '')
         syst_desc = '-'.join(infile.split('/')[11:13]).replace('_', '').replace('p1','').replace('p2','').replace('.', '')
         fnum      = infile.split('/')[-1].strip('l2_')
         outfile   ='%s/JLevel_%s_%s_%s_%s' % (outdir, nutype,eregime, syst_desc, fnum)
     elif 'data/nancy/i3/' in infile:
-        outdir    = '/data/user/jvillarreal/solar_atmospherics/event_selection/l3_a/data/nancy/h5/'
+        outdir    = '%s/nancy/h5/' % savedir
         fname   = infile.split('/')[-1].replace('i3.zst', 'h5')
         outfile = '%s/%s' % (outdir, fname)
     elif 'CORSIKA' in infile:
-        outdir    = '/data/user/jvillarreal/solar_atmospherics/event_selection/l3_a/data/corsika/i3/'
+        outdir    = '%s/corsika/i3/' % savedir
         fname  = infile.split('/')[-1].replace('bz2', 'zst')
         outfile = '%s/%s' % (outdir, fname)
     elif 'corsika' in infile:
-        outdir    = '/data/user/jvillarreal/solar_atmospherics/event_selection/l3_a/data/corsika/h5/'
+        outdir    = '%s/corsika/h5/' % savedir
         fname  = infile.split('/')[-1].replace('i3.zst', 'h5')
         outfile = '%s/%s' % (outdir, fname)
     elif 'GENIE' in infile:
-        outdir    = '/data/user/jvillarreal/solar_atmospherics/event_selection/l3_a/data/genie/i3/'
+        outdir    = '%s/genie/i3/' % savedir
         fname  = infile.split('/')[-1]
         outfile = '%s/%s' % (outdir, fname)
     elif 'genie' in infile:
-        outdir    = '/data/user/jvillarreal/solar_atmospherics/event_selection/l3_a/data/genie/h5/'
+        outdir    = '%s/genie/h5/' % savedir
         fname  = infile.split('/')[-1].replace('i3.zst', 'h5')
         outfile = '%s/%s' % (outdir, fname)
     elif 'exp' in infile:
-        print(1)
-        outdir    = '/data/user/jvillarreal/solar_atmospherics/event_selection/l3_a/data/exp_data/i3/'
+        outdir    = '%s/exp_data/i3/' % savedir
         fname  = infile.split('/')[-1]
         outfile = '%s/%s' % (outdir, fname)
     else:
