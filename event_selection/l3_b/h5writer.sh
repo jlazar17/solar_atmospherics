b9196b6c34d9902b1e49f6ec309970b51fe25801
jlazar17 <jeff.p.lazar@gmail.com>
2021-04-20 12:57:39 -0500

    change source to no combo

--- event_selection/l3_b/h5writer.sh
+++ event_selection/l3_b/h5writer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PYTHONPATH=$PYTHONPATH:/data/user/jlazar/.pylib
-source /data/user/jlazar/solar/solar_atmospherics/env.sh;
+source /data/user/jlazar/solar/solar_atmospherics/env_nocombo.sh;
 
 for INFILE in "$@";
 do
