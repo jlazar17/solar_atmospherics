25c814d889418415be3dae2f3faab7954a44a03d
jlazar17 <jeff.p.lazar@gmail.com>
2021-04-20 11:05:51 -0500

    Add env var to make_outfile_name

--- event_selection/l3_b/make_outfile_name.py
+++ event_selection/l3_b/make_outfile_name.py
@@ -1,8 +1,9 @@
 import os
 def make_outfile_name(infile):
+    outdir  = os.enviro.get('L3_B_SAVEDIR')
     infile  = infile.replace('=', '').replace('\\', '')
-    outfile = infile.replace('l3_a', 'l3_b')
-    outdir  = '/'.join(outfile.split('/')[:-1])
+    fname   = infile.split('/')[-1].replace('l3_a', 'l3_b')
+    outfile = '%s/%s' % (outdir, fname)
     if not os.path.isdir(outdir):
         os.makedirs(outdir)
     return outfile
